6717a4605d46b4ccdc410549f4d1a47f7e5c8d26
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2020-03-04 08:29:35 -0600

    offline working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,11 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/db.js",
-  "/favicon.ico",
   "/index.js",
   "/manifest.webmanifest",
-  "/style.css",
-  "/favicon.ico",
-  "/images/icon-72x72.png",
-  "/images/icon-96x96.png",
-  "/images/icon-128x128.png",
-  "/images/icon-144x144.png",
-  "/images/icon-152x152.png",
-  "/images/icon-192x192.png",
-  "/images/icon-384x384.png",
-  "/images/icon-512x512.png"
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
 ];
   
   
@@ -33,6 +24,7 @@ self.addEventListener("install", function(evt) {
   self.skipWaiting();
 });
 
+// activate
 self.addEventListener("activate", function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
@@ -52,7 +44,6 @@ self.addEventListener("activate", function(evt) {
 
 // fetch
 self.addEventListener("fetch", function(evt) {
-  // cache successful requests to the API
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
@@ -75,12 +66,11 @@ self.addEventListener("fetch", function(evt) {
     return;
   }
 
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
-    caches.match(evt.request).then(function(response) {
-      return response || fetch(evt.request);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
     })
   );
 });
-
